b65b47eaf4fc900bd0661b40658b98f9fc574027
Scott Gress <scott@pigandcow.com>
2012-10-09 16:50:05 -0500

    Updated timer code to fire on page load to check for sidebar Some CSS updates

--- ldengine.js
+++ ldengine.js
@@ -1,15 +1,19 @@
 var API_URL;
+var accountStatus;
 $(function() {
   var processMessageTimeout = null;
   var activeMessage = null;
+
+  processMessageTimeout = setInterval(processMessage,100);
   // Start monitoring changes to browser history, since GMail is an Ajax app
+
   $(window).bind("popstate", function(event) {
     clearTimeout(processMessageTimeout);
     // After the history changes, we need to wait for new content to load before we manipulate
     // the DOM, because the elements we want to manipulate may not immediately be available.
     // But, it looks like GMail turns off jQuery's global Ajax events (http://api.jquery.com/category/ajax/global-ajax-event-handlers/)
     // so for now we'll just act on a delay.
-    processMessageTimeout = setTimeout(processMessage,1000,event);
+    processMessageTimeout = setInterval(processMessage,100);
   });
 
   // Create a deferred object to wrap around a call to Chrome's
@@ -28,56 +32,78 @@ $(function() {
     var relatedEmails;
     // No sidebar?  Then we're not in a message.
     if ($('.y3').length === 0) {
+      console.log("No sidebar...");
       return;
     }
+    clearTimeout(processMessageTimeout);
     // Load the settings and all of the html templates
     $.when(
-      getSettings(),
-      $.get(chrome.extension.getURL("sidebar.tmpl"), function(data){$.templates('sidebarTemplate',data);}, 'html'),
-      $.get(chrome.extension.getURL("popup.tmpl"), function(data){$.templates('popupTemplate',data);}, 'html')
-    )
-    // Continue when loading is completed
-    .then(function() {
-      $('.ldengine').detach();
-      // Create the container
-      var block = $('<div class="ldengine"></div>');
-      // Place it on the page
-      placeBlock(block);
-      // Get the text of the current email
-      var text = $('.adP').text();
-      // Get the addresses of people this email was sent to
-      var toAddresses = [];
-      $('.hb').last().find('[email]').each(function(){toAddresses.push($(this).attr('email'));});
-      // Create the message to post to the server asking for related snippets
-      var postData = {
-        subject: $('.hP').text(),
-        body: $('.adP').last().text().replace(/\n/g,' '),
-        from: $('.h7').last().find('.gD').attr('email'),
-        to: toAddresses,
-        cc: [],
-        bcc: []
-      };
-      // Post the message to the server and get related snippets
-      $.post("http://"+API_URL+"/message/relatedSnippets",{Message:JSON.stringify(postData)},function(data){
-        relatedEmails = data;
-        // Format some stuff up in the data
-        for (var i = 0; i < relatedEmails.length; i++) {
-          var relatedEmail = relatedEmails[i];
-          relatedEmail.date = Date.parse(relatedEmail.date.replace(/\.\d+Z$/,'')).toString('MMM d');
-          relatedEmails[i] = relatedEmail;
+      getSettings()
+    ).then(function() {
+
+      $.when(
+        $.get("http://"+API_URL+"/account/status", function(data){accountStatus=data;}),
+        $.get(chrome.extension.getURL("sidebar.tmpl"), function(data){$.templates('sidebarTemplate',data);}, 'html'),
+        $.get(chrome.extension.getURL("popup.tmpl"), function(data){$.templates('popupTemplate',data);}, 'html')
+      )
+      // Continue when loading is completed
+      .then(function() {
+        $('#ldengine').detach();
+        // Create the container
+        var block = $('<div id="ldengine"></div>');
+        // Place it on the page
+        placeBlock(block);
+        // Check account status
+        /*
+        if (accountStatus.status == 'invalid') {
+          showLoginWindow(accountStatus.AuthUrl.url);
+          return;
         }
-        // Link the data from the server to the JsView template
-        $.link.sidebarTemplate( ".ldengine", relatedEmails );
-        // Ellipsize the related email snippets
-        $('.lde-email-result').dotdotdot();
-        // Hook up ze clicks
-        for (var i = 0; i < relatedEmails.length; i++) {
-          $($('.lde-email-result')[i]).data('data',relatedEmails[i]).click(onClickRelatedEmail);
-        }
-      },'json');
-    });
+        */
+        // Get the text of the current email
+        var text = $('.adP').text();
+        // Get the addresses of people this email was sent to
+        var toAddresses = [];
+        $('.hb').last().find('[email]').each(function(){toAddresses.push($(this).attr('email'));});
+        // Create the message to post to the server asking for related snippets
+        var postData = {
+          subject: $('.hP').text(),
+          body: $('.adP').last().text().replace(/\n/g,' '),
+          from: $('.h7').last().find('.gD').attr('email'),
+          to: toAddresses,
+          cc: [],
+          bcc: []
+        };
+        // Post the message to the server and get related snippets
+        $.post("http://"+API_URL+"/message/relatedSnippets",{Message:JSON.stringify(postData)},function(data){
+          relatedEmails = data;
+          // Format some stuff up in the data
+          for (var i = 0; i < relatedEmails.length; i++) {
+            var relatedEmail = relatedEmails[i];
+            relatedEmail.date = Date.parse(relatedEmail.date.replace(/\.\d+Z$/,'')).toString('MMM d');
+            relatedEmails[i] = relatedEmail;
+          }
+          // Link the data from the server to the JsView template
+          $.link.sidebarTemplate( "#ldengine", relatedEmails );
+          // Ellipsize the related email snippets
+          $('.lde-email-result').dotdotdot();
+          // Hook up ze clicks
+          for (var i = 0; i < relatedEmails.length; i++) {
+            $($('.lde-email-result')[i]).data('data',relatedEmails[i]).click(onClickRelatedEmail);
+          }
+        },'json');
+      });
+    })
   }
 });
+
+function showLoginWindow(url) {
+  var button = $('<button>Click here to log in</button>');
+  button.click(function(){
+    window.open(url,'lde-login','width=570,height=340,menubar=no,resizable=no,scrollbars=no,status=no,titlebar=no,toolbar=no,directories=no,channelmode=no');
+  });
+  $('#ldengine').append(button);
+}
 
 function onClickRelatedEmail() {
   activeMessage = $(this);
@@ -93,10 +119,10 @@ function popup(el) {
   maskMessageArea();
   // Popup the popup
   $('.adC').parent().append($('<div id="lde-popup"></div>'));
-  $.link.popupTemplate($('#lde-popup'),{});
+  //$.link.popupTemplate($('#lde-popup'),{});
   // Bind the scroll event of the sidebar so that the popup can track with the
   // message snippet it's attached to
-  $('.u5').bind('scroll',scrollPopup);
+  $('#ldengine').bind('scroll',scrollPopup);
   // Call the scroll callback to position the popup
   scrollPopup();
   // Get the related message data to fill the popup
@@ -106,7 +132,35 @@ function popup(el) {
 }
 
 function onReceivedRelatedMessageDetails(data) {
-  data.body = data.body.replace(/\\n/g,"<br/>");
+  // First, highlight the keywords.
+  // We'll extract them all first, so that
+  // the indexes don't get messed up when
+  // we replace the keyword with its highlighted
+  // equivalent.  In order to do that, we'll need
+  // to get them in order.
+  var keywordIndexes = [];
+  var keywords = {};
+  for (var i = 0; i < data.keywords.length; i++) {
+    keyword = data.keywords[i];
+    keywordIndexes.push(keyword.offset);
+    keywords[keyword.offset] = keyword;
+  }
+  keywordIndexes.sort();
+  var bodyParts = [];
+  var currentIndex = 0;
+  for (var i = 0; i < keywordIndexes.length; i++) {
+    var keywordIndex = keywordIndexes[i];
+    var keyword = keywords[keywordIndex];
+    bodyParts.push(data.body.substr(currentIndex,(keywordIndex - currentIndex)));
+    bodyParts.push('<span class="lde-keyword">'+data.body.substr(keywordIndex,keyword.keyword.length) + '</span>');
+    currentIndex = keywordIndex + keyword.keyword.length;
+  }
+  if (currentIndex < data.body.length) {
+    bodyParts.push(data.body.substr(currentIndex));
+  }
+  var body = bodyParts.join('').replace(/\n/g,"<br/>");
+  data.body = body;
+  data.date = Date.parse(data.date.replace(/\.\d+Z$/,'')).toString('MMM d');
   $.link.popupTemplate($('#lde-popup'),data);
   $('.lde-ajax-spinner').hide();
   $('.lde-popup-content').show();
@@ -117,7 +171,7 @@ function onReceivedRelatedMessageDetails(data) {
 }
 
 function removePopup() {
-  $('.u5').unbind('scroll',scrollPopup);
+  $('#ldengine').unbind('scroll',scrollPopup);
   $('#lde-popup').detach();
   maskMessageArea(false);
 }
@@ -140,15 +194,19 @@ function scrollPopup() {
       $('.lde-popup-arrow').hide();
     }
   }
-  else if (activeMessageTop + $('#lde-popup').height() > $('.u5').height()) {
+  else if (activeMessageTop + $('#lde-popup').height() > $('#ldengine').height()) {
     // If the message snippet is low enough that the popup would start to be
     // pushed off-screen, then peg the popup to the bottom of the message view
-    popupTop = ($('.u5').height() - $('#lde-popup').height());
-    // Move the arrow so that it's consistently pointing at the message snippet
-    $('.lde-popup-arrow').css('top',40-($('.u5').height() - (activeMessageTop + $('#lde-popup').height()))+'px');
-    // If the message snippet is out of view, hide the arrow
-    if (activeMessageTop > ($('.u5').height()-40)) {
-      $('.lde-popup-arrow').hide();
+    popupTop = ($('#ldengine').height() - $('#lde-popup').height());
+    if (popupTop < 0) {
+      popupTop = activeMessage.position().top;
+    } else {
+      // Move the arrow so that it's consistently pointing at the message snippet
+      $('.lde-popup-arrow').css('top',40-($('#ldengine').height() - (activeMessageTop + $('#lde-popup').height()))+'px');
+      // If the message snippet is out of view, hide the arrow
+      if (activeMessageTop > ($('#ldengine').height()-40)) {
+        $('.lde-popup-arrow').hide();
+      }
     }
   }
   // If the message is clearly in view, just move the popup along with it
@@ -157,22 +215,25 @@ function scrollPopup() {
   }
   // Account for the fact that the message snippet bar may not be at the top of the
   // sidebar (because there might be contact details or something else on top of it)
-  popupTop += $('.u5').position().top;
+  popupTop += $('#ldengine').position().top;
   $('#lde-popup').css('top',popupTop+'px');
 }
 
 function placeBlock(block) {
   // If there's an ad bar, replace it with our stuff
   if ($('.u5').length > 0) {
-    $('.u5').empty();
-    $('.u5').append(block);
+    console.log("replacing ads");
+    //$('#ldengine').empty();
+    $('.u5').replaceWith(block);
   }
   // Otherwise, if the sidebar has contact info at the top, insert our content after it
   else if ($('.anT').length > 0) {
+    console.log("Adding beneath contact info");
     block.insertAfter($('.anT'));
   }
   // Otherwise our content at the top of the sidebar
   else {
+    console.log("Adding to top of sidebar");
     $('.adC').prepend(block);
   }
 }
